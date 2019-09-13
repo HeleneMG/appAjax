9b224cfcb153917c0b45f201fe2f80b368c90579
HeleneMG <helenemouttetg@gmail.com>
2019-09-13 12:13:02 +0200

    search by year + error

--- app.js
+++ app.js
@@ -1,54 +1,40 @@
-let liste = document.querySelector("#listMovies");
-
+const liste = document.querySelector("#listMovies");
 const userClick = document.querySelector('#userSend');
-userClick.addEventListener('click', (event) => {
-    event.preventDefault();
-    liste.innerHTML = '';
-    const userInput = document.querySelector('#userInput').value;
-    getMovie(userInput);
-});
+const userInput = document.querySelector('#userInput');
+const userInputYear = document.querySelector("#userInputYear");
 
-const getMovie = (movie) => {
-    const URL = `http://www.omdbapi.com/?s=${movie}&apikey=ab3c648d`;
+//DATA MOVIE
+const dataMovie = (data) => {
+    if(data.Error){
+        alert(data.Error);
+    }
+    liste.innerHTML = '';
+    data.Search.forEach((movie) => {
+        liste.insertAdjacentHTML("beforeend", `
+            <li>
+                <img src="${movie.Poster}" alt="">
+                <h2 id="title">${movie.Title}</h2>
+                <p>${movie.Year}</p>
+            </li>
+            `);
+    });
+}
+
+//GET MOVIE
+const getMovie = (movie, year) => {
+    const URL = `http://www.omdbapi.com/?s=${movie}&y=${year}&apikey=ab3c648d`;
     fetch(URL)
         .then(response => response.json())
-        .then((data) => {
-            data.Search.forEach((movie) => {
-                liste.insertAdjacentHTML("beforeend", `
-            <li>
-                <img src="${movie.Poster}" alt="">
-                <h2 id="title">${movie.Title}</h2>
-                <p>${movie.Year}</p>
-            </li>
-            `);
-            });
-        });
+        .then(dataMovie)
+        .catch((error)=>{
+            console.log("erreur" + " " + error.message)
+        })
 };
-getMovie("harry potter");
+getMovie("disney");
 
-//DATE//DATE//DATE//DATE//DATE
-
-userClick2.addEventListener('click', (event) => {
+//CLICK RECHERCHE
+userClick.addEventListener('click', (event) => {
     event.preventDefault();
-    liste.innerHTML = '';
-    const userInputYear = document.querySelector('#userInputYear').value;
-    const userInput = document.querySelector('#userInput').value;
-    getMovie(userInput, userInputYear);
+    getMovie(userInput.value, userInputYear.value);
 });
 
-const getMovieYear = (movie) => {
-    const URL2 = `http://www.omdbapi.com/?t=${movie}&y=${movie.Year}&apikey=ab3c648d`;
-    fetch(URL2)
-        .then(response => response.json())
-        .then((data) => {
-            data.Search.forEach((movie) => {
-                liste.insertAdjacentHTML("beforeend", `
-            <li>
-                <img src="${movie.Poster}" alt="">
-                <h2 id="title">${movie.Title}</h2>
-                <p>${movie.Year}</p>
-            </li>
-            `);
-            });
-        });
-};
